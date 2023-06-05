abe014d92eb04ab14d3622ef35f0324671afaefa
austinsp135 <60867807+austinsp135@users.noreply.github.com>
2023-06-05 21:24:24 +0530

    Converter Functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+
+    path: '',
+
+    loadChildren: () => import('./components/converter/converter.component').then(m => m.ConverterComponent),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
