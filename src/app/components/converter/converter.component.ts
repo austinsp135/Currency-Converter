d78754def8b9ca05621c41b77569f70dbcc5191e
austinsp135 <60867807+austinsp135@users.noreply.github.com>
2023-06-05 16:17:13 +0530

    Initial commit

---
+++ src/app/components/converter/converter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-converter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.scss']
+})
+export class ConverterComponent {
+
+}
